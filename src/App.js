5a50f34d0ad978d38febc81f0b326e2c289110b6
sejyom <akxxkd01@gmail.com>
2023-11-30 14:59:45 +0900

    React Router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
- 
+import { BrowserRouter as Router, Routes, Route, } from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
+
 function App() {
-  return null;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/movie" element={<Detail />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,23 +1,19 @@
 import { useState, useEffect } from 'react'; 
-import Movie from "./components/Movie";
+import Movie from "../components/Movie";
 
 function Home() {
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
-  const getMovies = async() => {
-    // const response = await fetch(
-    //   "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year"
-    // );
-    // const json = await response.json();
+  const getMovies = async () => {
     const json = await (
       await fetch(
-        "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year"
-      )).json();
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+      )
+    ).json();
     setMovies(json.data.movies);
     setLoading(false);
   };
-  useEffect(() => getMovies(), []);
-
+  useEffect(() => {getMovies()}, []);
   
   return (
     <div>
